ced80687448dfefb45d795daf3e78a7f9be29e47
omar <jhonomar109@gmail.com>
2026-05-28 21:00:39 -0500

    feat: arquitectura limpia en proyecto

--- src/EssenceMvp.Mvc/Application/Services/IHealthCalculationService.cs
+++
@@ -1,8 +0,0 @@
-﻿using EssenceMvp.Mvc.Models;
-
-namespace EssenceMvp.Mvc.Application.Services;
-
-public interface IHealthCalculationService
-{
-    Task<ProjectHealthScore> CalculateProjectHealthAsync(int projectId);
-}
--- src/EssenceMvp.Mvc/Models/AlphaStateResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EssenceMvp.Mvc.Models;
-
-public class AlphaStateResult
-{
-    public short CurrentStateNumber { get; set; }
-    public string CurrentStateName { get; set; } = "";
-    public short MaxStateNumber { get; set; }
-}
